fab4bc476fd3d59ec2fd01960d8b038a548df0f7
Dov Czitter <dov_czitter@yahoo.com>
2022-05-31 16:49:21 -0400

    Update mostactivestocks_app.py
    Working PythonAnywhere version

--- mostactivestocks_app.py
+++ mostactivestocks_app.py
@@ -1,56 +1,77 @@
-from flask import Flask, render_template, request
-import requests
-import json
-import time
+from flask import Flask, render_template, request, send_from_directory
+import pytz, time
+from datetime import datetime
 from mostactivestocks import MostActiveStocks
+import os
 
 app = Flask(__name__)
 
+# ============================================
+# Required for requestResume() call in home.html.
+
+app.config['UPLOAD_FOLDER'] = os.getcwd()
+
+try:
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+except:
+    pass
+
+def get_files(target):
+    for file in os.listdir(target):
+        path = os.path.join(target, file)
+        if os.path.isfile(path):
+            yield (
+                file,
+                datetime.utcfromtimestamp(os.path.getmtime(path)),
+                os.path.getsize(path)
+            )
+@app.route('/download/<path:filename>')
+def download(filename):
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'],
+        filename,
+        as_attachment=True
+    )
+# ============================================
+
 @app.route('/')
+@app.route('/mostactivestocks')
 def handle_home():
-    return render_template('home.html')
-#   return MostActiveStocks.getHomeStr(request.url).encode("utf-8")
-
-@app.route('/beer')
-def handle_beer():
-    beer_url = 'https://api.punkapi.com/v2/beers/random'
-    r = requests.get(beer_url)
-    beer_json = r.json()
-
-    beer = {
-        'name':beer_json[0]['name'],
-        'abv':beer_json[0]['abv'],
-        'description':beer_json[0]['description'],
-        'foodpair':beer_json[0]['food_pairing'],
-    }
-#   print(beer)
-    return render_template('index.html',beer=beer)
-
-@app.route('/mostactivestocks', methods = ['GET', 'POST'])
-def handle_mostactivestocks():
-#   https://finance.yahoo.com/most-active
-#   https://finance.yahoo.com/most-active?count=300&offset=0
-#   MostActiveStocks.marketListInit()
-#   https://www.investing.com/equities/52-week-high
-#   https://www.investing.com/equities/52-week-low
-#   https://www.investing.com/equities/most-active-stocks
-#   https://www.investing.com/equities/top-stock-gainers
-#   https://www.investing.com/equities/top-stock-losers      
-
     url = request.url
-    print(f'url:{request.url}, url_root;{request.url_root}, base_url:{request.base_url} host_url:{request.host_url} root_url:{request.root_url}')
-    args = f'{url}'.split("=")
+    args = url.split("=")
     market = args[1] if len(args) > 1 else ''
-    homePath = f'{url}'
-    marketUrl = ''
-    if market == MostActiveStocks.marketYahooMostActives:
-#       -wip- '&' and '?' do not work, bash limitation        
-#       marketUrl = 'https://finance.yahoo.com/most-active?count=10&offset=0'
-        marketUrl = 'https://finance.yahoo.com/most-active'
-    elif market == MostActiveStocks.marketInvestingMostActives:
-        marketUrl = 'https://www.investing.com/equities/most-active-stocks'
-    if marketUrl == '':
-        return MostActiveStocks.getHomeStr(homePath).encode("utf-8")
-    else :
-        htmlTitle, tableData = MostActiveStocks.getJson(market,marketUrl)
-        return MostActiveStocks.getDataStr(htmlTitle, homePath, tableData).encode("utf-8")
+    htmlSrc = ''
+    info = {}
+    dtLocal = datetime.now().strftime("%d%b%Y:%H:%M:%S") + ' ' +  time.localtime().tm_zone
+    tzNY = pytz.timezone('America/New_York')
+    dtNY = datetime.now(tzNY).strftime("%d%b%Y:%H:%M:%S %Z")
+    yhaooHref = ''
+    investingHref = ''
+
+    if market == MostActiveStocks.YahooMostActivesName:
+        # --- Yahoo page ---
+        htmlSrc, data = MostActiveStocks.getData(MostActiveStocks.YahooMostActivesUrl)
+        headings = ( 'Symbol', 'Name', 'Price', 'Change', '% Change', 'Volume', '3 Month AvgVol', 'Monthly Cap', 'PE Ratio(TTM)' )
+        return render_template('data.html', market=market, headings=headings, data=data, htmlSrc=htmlSrc, MostActiveStocks=MostActiveStocks())
+
+    elif market == MostActiveStocks.InvestingMostActivesName:
+        # --- Investing page ---
+        htmlSrc, data = MostActiveStocks.getData(MostActiveStocks.InvestingMostActivesUrl)
+        headings = ( 'Name', 'Last', 'High', 'Low', 'Chg', 'Chg %', 'Vol', 'Time' )
+        return render_template('data.html', market=market, headings=headings, data=data, htmlSrc=htmlSrc, MostActiveStocks=MostActiveStocks())
+
+    else:
+        # --- HOME page ---
+        yhaooHref = f'{request.url}/mostactivestocks?market={MostActiveStocks.YahooMostActivesName}'
+        investingHref = f'{request.url}/mostactivestocks?market={MostActiveStocks.InvestingMostActivesName}'
+        if 'pythonanywhere' in url:
+            yhaooHref = f'{request.url}?market={MostActiveStocks.YahooMostActivesName}'
+            investingHref = f'{request.url}?market={MostActiveStocks.InvestingMostActivesName}'
+        info = {
+            'homePath' : url,
+            'YahooHref' : yhaooHref,
+            'InvestingHref' : investingHref,
+            'dtLocal' : dtLocal,
+            'dtNY' : dtNY,
+        }
+        return render_template('home.html', info=info, MostActiveStocks=MostActiveStocks())
